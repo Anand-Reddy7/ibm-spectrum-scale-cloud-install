acf050def7a352a327995b6660d067855657ab6f
Anand.Reddy7 <Anand.Reddy7@ibm.com>
2024-08-11 22:05:45 +0530

    Key Protect Support

--- resources/common/encryption_configuration/encryption_configuration.tf
+++ resources/common/encryption_configuration/encryption_configuration.tf
@@ -59,7 +59,7 @@ resource "null_resource" "perform_encryption_prepare" {
 }
 
 resource "null_resource" "perform_encryption_storage" {
-  count = (tobool(var.turn_on) == true && tobool(var.storage_cluster_encryption) == true && tobool(var.storage_cluster_create_complete) == true && tobool(var.remote_mount_create_complete) == true && tobool(var.create_scale_cluster) == true && var.scale_encryption_type) == "gklm" ? 1 : 0
+  count = (tobool(var.turn_on) == true && tobool(var.storage_cluster_encryption) == true && tobool(var.storage_cluster_create_complete) == true && tobool(var.remote_mount_create_complete) == true && tobool(var.create_scale_cluster) == true && var.scale_encryption_type == "gklm") ? 1 : 0
   provisioner "local-exec" {
     interpreter = ["/bin/bash", "-c"]
     command     = "/usr/local/bin/ansible-playbook -f 32 -i ${local.storage_inventory_path} ${local.encryption_cluster_playbook} -e scale_encryption_servers_dns=${local.scale_encryption_servers_dns}"
@@ -71,7 +71,7 @@ resource "null_resource" "perform_encryption_storage" {
 }
 
 resource "null_resource" "perform_encryption_compute" {
-  count = (tobool(var.turn_on) == true && tobool(var.compute_cluster_encryption) == true && tobool(var.compute_cluster_create_complete) == true && tobool(var.remote_mount_create_complete) == true && tobool(var.create_scale_cluster) == true && var.scale_encryption_type) == "gklm" ? 1 : 0
+  count = (tobool(var.turn_on) == true && tobool(var.compute_cluster_encryption) == true && tobool(var.compute_cluster_create_complete) == true && tobool(var.remote_mount_create_complete) == true && tobool(var.create_scale_cluster) == true && var.scale_encryption_type == "gklm") ? 1 : 0
   provisioner "local-exec" {
     interpreter = ["/bin/bash", "-c"]
     command     = "/usr/local/bin/ansible-playbook -f 32 -i ${local.compute_inventory_path} ${local.encryption_cluster_playbook} -e scale_encryption_servers_dns=${local.scale_encryption_servers_dns}"
@@ -83,7 +83,7 @@ resource "null_resource" "perform_encryption_compute" {
 }
 
 resource "null_resource" "perform_encryption_combined" {
-  count = (tobool(var.turn_on) == true && tobool(var.combined_cluster_encryption) == true && tobool(var.combined_cluster_create_complete) == true && tobool(var.create_scale_cluster) == true && var.scale_encryption_type) == "gklm" ? 1 : 0
+  count = (tobool(var.turn_on) == true && tobool(var.combined_cluster_encryption) == true && tobool(var.combined_cluster_create_complete) == true && tobool(var.create_scale_cluster) == true && var.scale_encryption_type == "gklm") ? 1 : 0
   provisioner "local-exec" {
     interpreter = ["/bin/bash", "-c"]
     command     = "/usr/local/bin/ansible-playbook -f 32 -i ${local.combined_inventory_path} ${local.encryption_cluster_playbook} -e scale_encryption_servers_dns=${local.scale_encryption_servers_dns}"
