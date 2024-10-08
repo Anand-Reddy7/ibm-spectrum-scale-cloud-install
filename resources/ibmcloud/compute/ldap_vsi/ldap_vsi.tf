577a1b2787ae1d4ccfb9ab6dc0a6240a26409efb
Anand.Reddy7 <Anand.Reddy7@ibm.com>
2024-10-08 12:17:31 +0530

    Migrating OpenLDAP serive from nslcd to SSSD
    Signed-off-by: Anand.Reddy7 <Anand.Reddy7@ibm.com>

--- resources/ibmcloud/compute/ldap_vsi/ldap_vsi.tf
+++ resources/ibmcloud/compute/ldap_vsi/ldap_vsi.tf
@@ -10,6 +10,7 @@ terraform {
   }
 }
 
+# Variable definitions
 variable "vsi_name_prefix" {}
 variable "vpc_id" {}
 variable "zones" {}
@@ -25,21 +26,29 @@ variable "vsi_meta_public_key" {}
 variable "ldap_basedns" {}
 variable "ldap_admin_password" {}
 
-
+# Create startup script template for configuring the instance
 data "template_file" "metadata_startup_script" {
-  template = <<EOF
+  template = <<-EOF
 #!/usr/bin/env bash
+
 USER=ubuntu
 basedns="${var.ldap_basedns}"
+logfile="/tmp/user_data.log"
 
 basedomain=$(echo "$basedns" | awk -F'.' '{print $1}')
 rootdomain=$(echo "$basedns" | awk -F'.' '{print $2}')
 
+# Install required software
 apt-get update -y
+apt-get install gnutls-bin ssl-cert nfs-common -y
+echo "Installed pre-reqiured softwares" >> $logfile
+
+# LDAP installation and configuration
 export DEBIAN_FRONTEND='non-interactive'
 echo -e "slapd slapd/root_password password ${var.ldap_admin_password}" |debconf-set-selections
 echo -e "slapd slapd/root_password_again password ${var.ldap_admin_password}" |debconf-set-selections
 apt-get install -y slapd ldap-utils
+echo "Installed OpenLDAP" >> $logfile
 
 echo -e "slapd slapd/internal/adminpw password ${var.ldap_admin_password}" |debconf-set-selections
 echo -e "slapd slapd/internal/generated_adminpw password ${var.ldap_admin_password}" |debconf-set-selections
@@ -54,20 +63,106 @@ dpkg-reconfigure slapd
 echo "BASE   dc=$basedomain,dc=$rootdomain" >> /etc/ldap/ldap.conf
 echo "URI    ldap://localhost" >> /etc/ldap/ldap.conf
 systemctl restart slapd
+echo "Started OpenLDAP service" >> $logfile
 
-sed -i -e "s/^/no-port-forwarding,no-agent-forwarding,no-X11-forwarding,command=\"echo \'Please login as the user \\\\\"$USER\\\\\" rather than the user \\\\\"root\\\\\".\';echo;sleep 10; exit 142\" /" /root/.ssh/authorized_keys
+# SSL certificate generation and OpenLDAP configuration
+echo "Creating SSL certificate" >> $logfile
+certtool --generate-privkey --sec-param High --outfile /etc/ssl/private/ldap_cakey.pem
+
+# Create CA template file
+cat <<-EOF_CA_INFO > /etc/ssl/ca.info
+cn = ${var.vsi_name_prefix}
+ca
+cert_signing_key
+expiration_days = 3650
+EOF_CA_INFO
+
+# Generate a self-signed CA certificate
+certtool --generate-self-signed \
+  --load-privkey /etc/ssl/private/ldap_cakey.pem \
+  --template /etc/ssl/ca.info \
+  --outfile /usr/local/share/ca-certificates/ldap_cacert.pem
+
+# Update CA certificates
+update-ca-certificates
+cp /usr/local/share/ca-certificates/ldap_cacert.pem /etc/ssl/certs/
+
+# Generate a private key and certificate for the LDAP server
+certtool --generate-privkey --sec-param High --outfile /etc/ssl/private/ldapserver_slapd_key.pem
+cat <<-EOF_LDAP_SERVER_INFO > /etc/ssl/ldapserver.info
+organization = ${var.vsi_name_prefix}
+cn = localhost
+tls_www_server
+encryption_key
+signing_key
+expiration_days = 3650
+EOF_LDAP_SERVER_INFO
+echo "Generated a private key and certificate for the LDAP server" >> $logfile
+
+# Generate a certificate for the LDAP server signed by the CA
+certtool --generate-certificate \
+  --load-privkey /etc/ssl/private/ldapserver_slapd_key.pem \
+  --load-ca-certificate /etc/ssl/certs/ldap_cacert.pem \
+  --load-ca-privkey /etc/ssl/private/ldap_cakey.pem \
+  --template /etc/ssl/ldapserver.info \
+  --outfile /etc/ssl/certs/ldapserver_slapd_cert.pem
+echo "Generated a certificate for the LDAP server signed by the CA" >> $logfile
+
+# Set proper permissions for the LDAP server private key
+chgrp openldap /etc/ssl/private/ldapserver_slapd_key.pem
+chmod 0640 /etc/ssl/private/ldapserver_slapd_key.pem
+gpasswd -a openldap ssl-cert
+echo "Applied TLS configuration" >> $logfile
+
+# Apply TLS configuration and restart slapd service
+systemctl restart slapd.service
+echo "Restarted the OpenLDAP service" >> $logfile
+
+
+# Configure OpenLDAP to use TLS
+cat <<-EOF_LDIF > /etc/ssl/certinfo.ldif
+dn: cn=config
+add: olcTLSCACertificateFile
+olcTLSCACertificateFile: /etc/ssl/certs/ldap_cacert.pem
+-
+add: olcTLSCertificateFile
+olcTLSCertificateFile: /etc/ssl/certs/ldapserver_slapd_cert.pem
+-
+add: olcTLSCertificateKeyFile
+olcTLSCertificateKeyFile: /etc/ssl/private/ldapserver_slapd_key.pem
+EOF_LDIF
+
+ldapmodify -Y EXTERNAL -H ldapi:/// -f /etc/ssl/certinfo.ldif
+sed -i 's|SLAPD_SERVICES="ldap:/// ldapi:///"|SLAPD_SERVICES="ldap:/// ldapi:/// ldaps:///"|g' /etc/default/slapd
+echo "Configured OpenLDAP to use TLS" >> >> $logfile
+
+# Update LDAP client configuration
+cat <<-EOF_LDAP_CONF > /etc/ldap/ldap.conf
+BASE   dc=$basedomain,dc=$rootdomain
+URI    ldap://localhost
+TLS_CACERT /etc/ssl/certs/ldap_cacert.pem
+TLS_REQCERT allow
+EOF_LDAP_CONF
+
+systemctl restart slapd.service
+echo "SSL creation completed" >> "$logfile"
+
+# Configure SSH settings
+sed -i -e "s/^/no-port-forwarding,no-agent-forwarding,no-X11-forwarding,command=\"echo 'Please login as the user \\\\\"$USER\\\\\" rather than the user \\\\\"root\\\\\".';echo;sleep 10; exit 142\" /" /root/.ssh/authorized_keys
 sed -i "s/#MaxSessions 10/MaxSessions 32/" /etc/ssh/sshd_config
 sed -i "s/#MaxStartups 10:30:100/MaxStartups 30:30:100/" /etc/ssh/sshd_config
 systemctl restart sshd.service
+echo "Restarted SSHD service" >> "$logfile"
 
-#Copying SSH for passwordless authentication
-echo "${var.vsi_meta_private_key}" > ~/.ssh/id_rsa
-chmod 600 ~/.ssh/id_rsa
-echo "${var.vsi_meta_public_key}" >> ~/.ssh/authorized_keys
-echo "StrictHostKeyChecking no" >> ~/.ssh/config
+# Set up passwordless SSH authentication
+echo "${var.vsi_meta_private_key}" > /home/ubuntu/.ssh/id_rsa
+chmod 600 /home/ubuntu/.ssh/id_rsa
+echo "${var.vsi_meta_public_key}" >> /home/ubuntu/.ssh/authorized_keys
+echo "StrictHostKeyChecking no" >> /home/ubuntu/.ssh/config
 EOF
 }
 
+# Resource definition for the VSI instance
 resource "ibm_is_instance" "itself" {
   name    = var.vsi_name_prefix
   image   = var.vsi_image_id
@@ -90,6 +185,7 @@ resource "ibm_is_instance" "itself" {
   }
 }
 
+# Outputs for VSI instance information
 output "vsi_id" {
   value = ibm_is_instance.itself.id
 }
